34e70d57f70ff99c4b7f7e093d5add2489c61ff2
abhishekkumarabhi108-ui <abhishekkumarabhi108@gmail.com>
2026-08-03 20:13:24 +0530

    Time: 0 ms (100%), Space: 8.8 MB (99.84%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.c
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+
+    // Both are NULL
+    if (p == NULL && q == NULL) {
+        return true;
+    }
+
+    // One is NULL and other is not
+    if (p == NULL || q == NULL) {
+        return false;
+    }
+
+    // Values are different
+    if (p->val != q->val) {
+        return false;
+    }
+
+    // Check left and right subtrees
+    return isSameTree(p->left, q->left) &&
+           isSameTree(p->right, q->right);
+}
