884755baf8214b6d49403e639dd5b7fa9b36013a
abhishekkumarabhi108-ui <abhishekkumarabhi108@gmail.com>
2026-08-06 22:54:33 +0530

    Time: 0 ms (100%), Space: 8.7 MB (80.4%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.c
@@ -0,0 +1,20 @@
+#include <string.h>
+
+int lengthOfLastWord(char* s) {
+
+    int i = strlen(s) - 1;
+    int length = 0;
+
+    // Skip trailing spaces
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+
+    // Count characters of the last word
+    while (i >= 0 && s[i] != ' ') {
+        length++;
+        i--;
+    }
+
+    return length;
+}
