0d5e62686a51cde3c826240bea25045de216200d
abhishekkumarabhi108-ui <abhishekkumarabhi108@gmail.com>
2026-08-07 17:16:53 +0530

    Time: 0 ms (100%), Space: 9 MB (17.62%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+int strStr(char* haystack, char* needle) {
+
+    int n = strlen(haystack);
+    int m = strlen(needle);
+
+    if (m == 0) {
+        return 0;
+    }
+
+    for (int i = 0; i <= n - m; i++) {
+
+        int j = 0;
+
+        while (j < m && haystack[i + j] == needle[j]) {
+            j++;
+        }
+
+        // Entire needle matched
+        if (j == m) {
+            return i;
+        }
+    }
+
+    return -1;
+}
