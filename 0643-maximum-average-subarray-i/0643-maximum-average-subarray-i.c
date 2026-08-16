14f5d2aab86972afd384c997aab69bf8dfc81bda
abhishekkumarabhi108-ui <abhishekkumarabhi108@gmail.com>
2026-08-16 18:06:12 +0530

    Time: 0 ms (100%), Space: 16.9 MB (97.06%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.c
@@ -0,0 +1,23 @@
+double findMaxAverage(int* nums, int numsSize, int k) {
+
+    int sum = 0;
+
+    // First window
+    for (int i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    int maxSum = sum;
+
+    // Slide the window
+    for (int i = k; i < numsSize; i++) {
+
+        sum = sum + nums[i] - nums[i - k];
+
+        if (sum > maxSum) {
+            maxSum = sum;
+        }
+    }
+
+    return (double)maxSum / k;
+}
