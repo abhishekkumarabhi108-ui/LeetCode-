14820025ded30fd5c1df5298b91d1d26bd09957d
abhishekkumarabhi108-ui <abhishekkumarabhi108@gmail.com>
2026-08-17 17:04:19 +0530

    Time: 1194 ms (5.56%), Space: 10.2 MB (83.33%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.c
+++ 1563-stone-game-v/1563-stone-game-v.c
@@ -1,63 +1,56 @@
 #include <stdlib.h>
 
-int stoneGameV(int* stoneValue, int stoneValueSize) {
+int stoneGameV(int* stoneValue, int n) {
 
-    int n = stoneValueSize;
+    int prefix[501] = {0};
+    int dp[501][501] = {0};
 
     // Prefix sum
-    int* prefix = (int*)malloc((n + 1) * sizeof(int));
-
-    prefix[0] = 0;
-
     for (int i = 0; i < n; i++) {
         prefix[i + 1] = prefix[i] + stoneValue[i];
     }
 
-    // dp[i][j] = maximum score from i to j
-    int dp[500][500] = {0};
-
-    // Length of the subarray
+    // Length of subarray
     for (int len = 2; len <= n; len++) {
 
         for (int i = 0; i + len <= n; i++) {
 
             int j = i + len - 1;
 
-            // Try every possible split
+            // Try every split
             for (int k = i; k < j; k++) {
 
                 int left = prefix[k + 1] - prefix[i];
                 int right = prefix[j + 1] - prefix[k + 1];
 
                 if (left < right) {
-                    int value = left + dp[i][k];
 
-                    if (value > dp[i][j])
-                        dp[i][j] = value;
+                    int score = left + dp[i][k];
+
+                    if (score > dp[i][j])
+                        dp[i][j] = score;
                 }
                 else if (right < left) {
-                    int value = right + dp[k + 1][j];
 
-                    if (value > dp[i][j])
-                        dp[i][j] = value;
+                    int score = right + dp[k + 1][j];
+
+                    if (score > dp[i][j])
+                        dp[i][j] = score;
                 }
                 else {
-                    int value1 = left + dp[i][k];
-                    int value2 = right + dp[k + 1][j];
 
-                    if (value1 > dp[i][j])
-                        dp[i][j] = value1;
+                    int score1 = left + dp[i][k];
+                    int score2 = right + dp[k + 1][j];
 
-                    if (value2 > dp[i][j])
-                        dp[i][j] = value2;
+                    if (score1 > dp[i][j])
+                        dp[i][j] = score1;
+
+                    if (score2 > dp[i][j])
+                        dp[i][j] = score2;
                 }
             }
         }
     }
 
-    int answer = dp[0][n - 1];
-
-    free(prefix);
-
-    return answer;
+    return dp[0][n - 1];
 }
